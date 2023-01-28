e9024ed4b7f5baa396bea6e29e47d29089d97891
Vitor Santos <pireshugo737@gmail.com>
2023-01-28 16:28:17 -0300

    update: project rework

---
+++ README.md
@@ -0,0 +1,21 @@
+# Chat de Mensagens
+
+## 💻 Sobre
+
+Aplicação web de mensagens.
+
+Treino de desenvolvimento de aplicação web com forte manipulação do DOM e tecnologias como SASS (Syntactically Awesome Style Sheets) e TS (TypeScript).
+
+## ⭐ Conceitos e Tecnologias
+
+- HypeText Markup Language (HTML5);
+- Cascading Style Sheets (CSS3);
+- Syntactically Awesome Style Sheets (SASS);
+- JavaScript (JS);
+- TypeScript (TS); e
+- LocalStorage.
+
+## Visualização
+
+<img src="./.github/view-project.png">
+<a href="https://vitorsantos920.github.io/chatMessage">Visualizar Projeto Online</a>
--- assets/ts/script.ts
+++ assets/ts/script.ts
@@ -1,12 +1,26 @@
-"use strict";
-const iptMsgUser1 = document.getElementById("ipt-content-msg--user-1");
-const sendMSgUser1 = document.querySelector(".send-msg--user-1");
-const iptMsgUser2 = document.getElementById("ipt-content-msg--user-2");
-const sendMSgUser2 = document.querySelector(".send-msg--user-2");
-const chat = document.querySelector(".messages--chat");
-let messages = [];
+const iptMsgUser1: HTMLInputElement = <HTMLInputElement>(
+  document.getElementById("ipt-content-msg--user-1")
+);
 
-function keyPress(k, user) {
+const sendMSgUser1: HTMLImageElement = <HTMLImageElement>(
+  document.querySelector(".send-msg--user-1")
+);
+
+const iptMsgUser2: HTMLInputElement = <HTMLInputElement>(
+  document.getElementById("ipt-content-msg--user-2")
+);
+
+const sendMSgUser2: HTMLImageElement = <HTMLImageElement>(
+  document.querySelector(".send-msg--user-2")
+);
+
+const chat: HTMLElement = <HTMLElement>(
+  document.querySelector(".messages--chat")
+);
+
+let messages: object[] = [];
+
+function keyPress(k: any, user: number): void {
   if (k.key === "Enter") {
     if (user == 1) {
       message(1, iptMsgUser1.value, "content-message--user-1");
@@ -18,7 +32,7 @@ function keyPress(k, user) {
   }
 }
 
-function identifyUser(user) {
+function identifyUser(user: number): void {
   if (user == 1) {
     message(1, iptMsgUser1.value, "content-message--user-1");
     iptMsgUser1.value = "";
@@ -28,20 +42,26 @@ function identifyUser(user) {
   }
 }
 
-function message(user, contentMsg, classP) {
+function message(user: number, contentMsg: string, classP: string): void {
   let p = document.createElement("p");
   p.innerHTML = contentMsg;
   p.classList.add(classP);
+
   messages.push({
     user: user,
     msg: contentMsg,
   });
+
   localStorage.setItem("messages", JSON.stringify(messages));
+
   chat.append(p);
 }
 
-let userMessages = JSON.parse(localStorage.getItem("messages")) || [];
+let userMessages: any[] =
+  JSON.parse(<string>localStorage.getItem("messages")) || [];
+
 let classP;
+
 for (let i = 0; i < userMessages.length; i++) {
   userMessages[i].user == 1
     ? (classP = "content-message--user-1")
