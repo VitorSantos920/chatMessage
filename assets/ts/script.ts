97d4aff0628b7f3f2de857d32cfb34bc19b70d7a
Vitor Santos <pireshugo737@gmail.com>
2023-05-08 16:34:48 -0300

    refactor: typescript interface for params

--- assets/ts/script.ts
+++ assets/ts/script.ts
@@ -1,70 +1,95 @@
 const iptMsgUser1: HTMLInputElement = <HTMLInputElement>(
-  document.getElementById("ipt-content-msg--user-1")
+    document.getElementById('ipt-content-msg--user-1')
 );
 
 const sendMSgUser1: HTMLImageElement = <HTMLImageElement>(
-  document.querySelector(".send-msg--user-1")
+    document.querySelector('.send-msg--user-1')
 );
 
 const iptMsgUser2: HTMLInputElement = <HTMLInputElement>(
-  document.getElementById("ipt-content-msg--user-2")
+    document.getElementById('ipt-content-msg--user-2')
 );
 
 const sendMSgUser2: HTMLImageElement = <HTMLImageElement>(
-  document.querySelector(".send-msg--user-2")
+    document.querySelector('.send-msg--user-2')
 );
 
 const chat: HTMLElement = <HTMLElement>(
-  document.querySelector(".messages--chat")
+    document.querySelector('.messages--chat')
 );
 
 let messages: object[] = [];
 
 function keyPress(k: any, user: number): void {
-  if (k.key === "Enter") {
-    if (user == 1) {
-      message(1, iptMsgUser1.value, "content-message--user-1");
-      iptMsgUser1.value = "";
-    } else {
-      message(2, iptMsgUser2.value, "content-message--user-2");
-      iptMsgUser2.value = "";
+    if (k.key === 'Enter') {
+        if (user == 1) {
+            message({
+                user: 1,
+                contentMsg: iptMsgUser1.value,
+                classP: 'content-message--user-1',
+            });
+            iptMsgUser1.value = '';
+        } else {
+            message({
+                user: 2,
+                contentMsg: iptMsgUser2.value,
+                classP: 'content-message--user-2',
+            });
+            iptMsgUser2.value = '';
+        }
     }
-  }
 }
 
 function identifyUser(user: number): void {
-  if (user == 1) {
-    message(1, iptMsgUser1.value, "content-message--user-1");
-    iptMsgUser1.value = "";
-  } else {
-    message(2, iptMsgUser2.value, "content-message--user-2");
-    iptMsgUser2.value = "";
-  }
+    if (user == 1) {
+        message({
+            user: 1,
+            contentMsg: iptMsgUser1.value,
+            classP: 'content-message--user-1',
+        });
+        iptMsgUser1.value = '';
+    } else {
+        message({
+            user: 2,
+            contentMsg: iptMsgUser2.value,
+            classP: 'content-message--user-2',
+        });
+        iptMsgUser2.value = '';
+    }
 }
 
-function message(user: number, contentMsg: string, classP: string): void {
-  let p = document.createElement("p");
-  p.innerHTML = contentMsg;
-  p.classList.add(classP);
+interface IMessageParams {
+    user: number;
+    contentMsg: string;
+    classP: string;
+}
+function message({ user, contentMsg, classP }: IMessageParams): void {
+    let p = document.createElement('p');
+    p.innerHTML = contentMsg;
+    p.classList.add(classP);
 
-  messages.push({
-    user: user,
-    msg: contentMsg,
-  });
+    messages.push({
+        user: user,
+        msg: contentMsg,
+    });
 
-  localStorage.setItem("messages", JSON.stringify(messages));
+    localStorage.setItem('messages', JSON.stringify(messages));
 
-  chat.append(p);
+    chat.append(p);
 }
 
 let userMessages: any[] =
-  JSON.parse(<string>localStorage.getItem("messages")) || [];
+    JSON.parse(<string>localStorage.getItem('messages')) || [];
 
 let classP;
 
 for (let i = 0; i < userMessages.length; i++) {
-  userMessages[i].user == 1
-    ? (classP = "content-message--user-1")
-    : (classP = "content-message--user-2");
-  message(userMessages[i].user, userMessages[i].msg, classP);
+    userMessages[i].user == 1
+        ? (classP = 'content-message--user-1')
+        : (classP = 'content-message--user-2');
+    message({
+        user: userMessages[i].user,
+        contentMsg: userMessages[i].msg,
+        classP: classP,
+    });
 }
